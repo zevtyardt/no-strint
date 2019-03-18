94de0419ad30e99c170969dd967c5cf5d47173ca
zevtyardt <apklover76@gmail.com>
2019-03-18 20:30:09 +0700

    update tests

---
+++ tests/test.py
@@ -0,0 +1,20 @@
+import random
+
+a = 0
+b = 1.2
+c = 'abc..'
+d = r'can\'t'
+u7 = 8 # variable with int
+
+if c[0] == random.choice(['a', 'b']):
+    print ('tes', 'again')
+else:
+    print (a, b, c, d)
+
+print '\nthis\nnewline\n'
+
+print '\x1b[32mwith \033[34mcolor\033[0m'
+
+print '\twith tab'
+
+print "u7"
